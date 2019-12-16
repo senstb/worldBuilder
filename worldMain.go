f0e1b1b19b4f52f66397393d04008d3a32d472a8
senstb <senst.b@gmail.com>
2019-12-15 16:57:05 -0800

    splitting actions from main menu functionality

--- worldMain.go
+++ worldMain.go
@@ -3,27 +3,10 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math/rand"
 	"os"
 	"os/exec"
 	"strings"
 )
-
-// Action Section
-func attack(user Player, monster Monster) {
-	attackValue := rand.Intn(user.Attack - 1)
-	fmt.Println(user.Name, "attacked for", attackValue, "damage!")
-	monster.Health = monster.Health - attackValue
-}
-
-func run(user Player, enemy Monster) {
-	if rand.Intn(2-0) == 0 {
-		fmt.Println("You got away from the", enemy)
-	} else {
-		fmt.Println("You couldn't get away from the", enemy)
-	}
-
-}
 
 //Main menu functionality
 func userOptions() {
