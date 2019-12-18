0da1cedbacee32eb63bc84a9dee0cbc1be3d7efa
senstb <senst.b@gmail.com>
2019-12-18 06:41:31 -0800

    added monster attack; need to revise health stat saving

--- go/src/worldMain/actions.go
+++ go/src/worldMain/actions.go
@@ -16,11 +16,29 @@ func defend(user *Player, monster *Monster) {
 	fmt.Println("You defend for", defendValue, "damage!")
 }
 
-func run(user *Player, enemy *Monster) bool {
+func run(user *Player, monster *Monster) bool {
 	if rand.Intn(2-0) == 0 {
-		fmt.Println("You got away from the", enemy.Name)
+		fmt.Println(user.Name, "got away from the", monster.Name)
 		return true
 	}
-	fmt.Println("You couldn't get away from the", enemy.Name)
+	fmt.Println(user.Name, "couldn't get away from the", monster.Name)
+	return false
+}
+func monsterAttack(monster *Monster, user *Player) {
+	attackValue := rand.Intn(monster.Attack - 0)
+	fmt.Println(monster.Name, "attacked for", attackValue, "damage!")
+	monster.Health = user.Health - attackValue
+}
+
+func monsterDefend(monster *Monster, user *Player) {
+	defendValue := rand.Intn(monster.Attack - 1)
+	fmt.Println("You defend for", defendValue, "damage!")
+}
+func monsterRun(monster *Monster, user *Player) bool {
+	if rand.Intn(2-0) == 0 {
+		fmt.Println(monster.Name, "got away from", user.Name)
+		return true
+	}
+	fmt.Println(monster.Name, "couldn't get away from", user.Name)
 	return false
 }
--- go/src/worldMain/worldMain.go
+++ go/src/worldMain/worldMain.go
@@ -17,6 +17,7 @@ func userDecision(input string, user *Player, monster *Monster) bool {
 	case "1":
 		fmt.Println("You chose to attack the", monster.Name)
 		attack(user, monster)
+		// Monster health not staying static
 		fmt.Println("Monster Health:", monster.Health)
 		if monster.Health <= 0 {
 			fmt.Println("You defeated the", monster.Name)
@@ -36,6 +37,24 @@ func userDecision(input string, user *Player, monster *Monster) bool {
 	default:
 		fmt.Println("No options selected")
 		return false
+	}
+}
+
+//Needs more work to finalize monsterDecision making and actions
+func monsterDecision(monster *Monster, user *Player) bool {
+	if monster.Health > (monster.Health / 2) {
+		monsterAttack(monster, user)
+		fmt.Println(user.Name, "health:", user.Health)
+		if user.Health <= 0 {
+			fmt.Println("You were defeated by the", monster.Name)
+			return true
+		}
+		return false
+	} else if monster.Health > (monster.Health / 4) {
+		monsterDefend(monster, user)
+		return false
+	} else {
+		return monsterRun(monster, user)
 	}
 }
 
@@ -61,5 +80,6 @@ func main() {
 		print(">>")
 		nextChoice := readUserInput()
 		exit = userDecision(nextChoice, &user, &monsterObj)
+		exit = monsterDecision(&monsterObj, &user)
 	}
 }
