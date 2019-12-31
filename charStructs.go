81ffa3b66a3899573b6d28ef3e42987a1fb82bb8
senstb <senst.b@gmail.com>
2019-12-31 06:45:41 -0800

    Updated Structs with objects/inheritance to Character struct

--- charStructs.go
+++ charStructs.go
@@ -4,34 +4,48 @@ import (
 	"math/rand"
 )
 
-//Monster struct
-type Monster struct {
+//Character struct
+type Character struct {
 	Name           string
 	Health, Attack int
-	//Monster character for game
+}
+
+//Monster struct
+type Monster struct {
+	Character
+	Type string
 }
 
 //Player struct
 type Player struct {
-	Name                  string
-	Health, Attack, Level int
+	Character
+	Level int
+}
+
+func (c *Character) getName() string {
+	return c.Name
+}
+
+func (c *Character) getAttack() int {
+	return c.Attack
+}
+
+func (c *Character) getHealth() int {
+	return c.Health
 }
 
 func initUser(name string) Player {
 	var player = Player{
-		Name:   name,
-		Health: rand.Intn(20 - 5),
-		Attack: rand.Intn(5 - 1),
-		Level:  1,
+		Character: Character{Name: name, Health: rand.Intn(20 - 5), Attack: rand.Intn(5 - 1)},
+		Level:     1,
 	}
 	return player
 }
 
 func initMonster() Monster {
 	var monster = Monster{
-		Name:   "Skeleton",
-		Health: rand.Intn(10 - 1),
-		Attack: rand.Intn(3 - 0),
+		Character: Character{Health: rand.Intn(10 - 1), Attack: rand.Intn(3 - 0)},
+		Type:      "Skeleton",
 	}
 	return monster
 }
